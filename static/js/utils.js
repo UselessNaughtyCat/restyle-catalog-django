80c0cc4c764a82ecdbbc6e566ce9167e4d0ea752
UselessNaughtyCat <PichuginN1999@yandex.ru>
2018-05-29 00:23:29 +0500

    memes

--- static/js/utils.js
+++ static/js/utils.js
@@ -21,28 +21,6 @@ function getCookie(name) {
     return cookieValue;
 }
 
-
 function responseJSON(response) {
     return response.json()
 }
-
-// let countForYobaTitle = 0;
-// function clickTitleToYobaTitle(e) {
-//     if (countForYobaTitle === 10) {
-//         this.innerHTML = "АЯЗ!";
-//         document.getElementById("t2").innerHTML = "Заебошь короч ахуенный слоган для рестайла.";
-//         document.getElementById("t3").innerHTML = "А то ты опять будешь бомбить, что я написал какой-то дуратский текст.";
-//     } else if (countForYobaTitle > 10) {
-//         this.innerHTML = "ReSTYLE";
-//         document.getElementById("t2").innerHTML = "Easy way to customize your web pages";
-//         document.getElementById("t3").innerHTML = "Change the appearance of your favorite web pages as you need";
-//         countForYobaTitle = 0;
-//     }
-//     console.log(10 - countForYobaTitle);
-//     countForYobaTitle++;
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     let t1 = document.getElementById("t1");
-//     t1.onclick = clickTitleToYobaTitle;
-// })
